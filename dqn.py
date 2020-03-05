59ae25eff5f4b53a6cbfc489f0567174ecc88c16
Garrett Springer <ghsprin@g.clemson.edu>
2020-03-05 17:48:40 -0500

    add q_iteration func

--- dqn.py
+++ dqn.py
@@ -1,5 +1,23 @@
 import numpy as np
 
+def q_iteration(env, model, state, iteration, memory):
+    # Choose epsilon based on the iteration
+    epsilon = get_epsilon_for_iteration(iteration)
+
+    # Choose the action 
+    if random.random() < epsilon:
+        action = env.action_space.sample()
+    else:
+        action = choose_best_action(model, state)
+
+    # Play one game iteration (note: according to the next paper, you should actually play 4 times here)
+    new_frame, reward, is_done, _ = env.step(action)
+    memory.add(state, action, new_frame, reward, is_done)
+
+    # Sample and fit
+    batch = memory.sample_batch(32)
+    fit_batch(model, batch)
+
 def fit_batch(model, gamma, start_states, actions, rewards, next_states, is_terminal):
     """Do one deep Q learning iteration.
     
