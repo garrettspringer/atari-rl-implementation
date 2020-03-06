5dc2c64fc5508a0ddd0cd4ca2291dd58f9f3d708
Garrett Springer <ghsprin@g.clemson.edu>
2020-03-06 14:34:37 -0500

    allow for multiple iterations

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import gym
 from model import atari_model
+import dqn
 
 env = gym.make('BreakoutDeterministic-v4')
 # returns the starting frame
 frame = env.reset()
 env.render()
 
-is_done = False
-while not is_done:
-  # Number of actions in the game
-  n_actions = env.action_space
-  # Create model
-  model = atari_model(n_actions)
-  # Perform a random action, returns the new frame, reward and whether the game is over
-  frame, reward, is_done, _ = env.step(env.action_space.sample())
-  env.render()
+for _ in range(10000):
+  is_done = False
+  while not is_done:
+    # Number of actions in the game
+    n_actions = env.action_space
+    # Create model
+    model = atari_model(n_actions)
+    # Select an action
+    action = model.choose_best_action(model)   
+
+    # Perform a random action, returns the new frame, reward and whether the game is over
+    frame, reward, is_done, _ = env.step(env.action_space.sample())
+    env.render()
+
+env.close()
