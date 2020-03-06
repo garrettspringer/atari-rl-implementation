0c32269ed6ca1106d03296efe0e0c75c315ac827
Garrett Springer <ghsprin@g.clemson.edu>
2020-03-06 14:06:47 -0500

    build atari model based on n_actions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import gym
+from model import atari_model
 
 env = gym.make('BreakoutDeterministic-v4')
 # returns the starting frame
@@ -7,6 +8,10 @@ env.render()
 
 is_done = False
 while not is_done:
+  # Number of actions in the game
+  n_actions = env.action_space
+  # Create model
+  model = atari_model(n_actions)
   # Perform a random action, returns the new frame, reward and whether the game is over
   frame, reward, is_done, _ = env.step(env.action_space.sample())
   env.render()
