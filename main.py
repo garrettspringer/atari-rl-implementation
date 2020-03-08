b301682d4a976920bd029a98aab86d1dd02bb95e
Garrett Springer <ghsprin@g.clemson.edu>
2020-03-07 23:07:17 -0500

    make the action space generic

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from model import atari_model
 env = gym.make('BreakoutDeterministic-v4')
 env.render()
 
-for _ in range(10000):
+# Create model
+# How do i make this into an int? env.action_space
+model = atari_model(len(env.unwrapped.get_action_meanings()))
+
+for _ in range(1000):
   # Return to the starting frame
   frame = env.reset()
   is_done = False
   while not is_done:
-    # Number of actions in the game
-    n_actions = env.action_space
-    # Create model
-    model = atari_model(n_actions)
     # Select an action
-    action = model.choose_best_action(frame)   
+    #action = model.choose_best_action(frame) 
     # Perform a random action, returns the new frame, reward and whether the game is over
-    frame, reward, is_done, _ = env.step(action)
+    frame, reward, is_done, _ = env.step(env.action_space.sample())
     env.render()
 
 env.close()
