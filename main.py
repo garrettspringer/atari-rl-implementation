070e4ac8ba8234d95f64c73a5e6a97a1c3538649
Garrett Springer <ghsprin@g.clemson.edu>
2020-03-08 00:44:05 -0500

    initialize ring buffer

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ env = gym.make('BreakoutDeterministic-v4')
 env.render()
 
 # Create model
-# How do i make this into an int? env.action_space
 model = atari_model(len(env.unwrapped.get_action_meanings()))
+
+max_num_memories = 1000000
+memory_storage = RingBuf(max_num_memories)
 
 for i in range(1000):
   # Return to the starting frame
   frame = env.reset()
   is_done = False
   while not is_done:
-    model.q_iteration(env, frame, i, RingBuf)
+    model.q_iteration(env, frame, i, memory_storage)
 
 env.close()
