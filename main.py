14fa94243d18e5ae8f24f96d785b1efe2c92ec43
Garrett Springer <ghsprin@g.clemson.edu>
2020-03-08 00:37:25 -0500

    add ring buffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gym
 from model import atari_model
+from memory import RingBuf
 
 env = gym.make('BreakoutDeterministic-v4')
 env.render()
@@ -8,15 +9,11 @@ env.render()
 # How do i make this into an int? env.action_space
 model = atari_model(len(env.unwrapped.get_action_meanings()))
 
-for _ in range(1000):
+for i in range(1000):
   # Return to the starting frame
   frame = env.reset()
   is_done = False
   while not is_done:
-    # Select an action
-    #action = model.choose_best_action(frame) 
-    # Perform a random action, returns the new frame, reward and whether the game is over
-    frame, reward, is_done, _ = env.step(env.action_space.sample())
-    env.render()
+    model.q_iteration(env, frame, i, RingBuf)
 
 env.close()
