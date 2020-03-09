02d587d08d1019b5a4c65dc24f187a52e2592cc6
Garrett Springer <ghsprin@g.clemson.edu>
2020-03-09 19:41:19 -0400

    reformat numpy array for choosing best action

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ env = gym.make('BreakoutDeterministic-v4')
 env.render()
 
 # Create model
-model = atari_model(len(env.unwrapped.get_action_meanings()))
+model = atari_model(env.unwrapped.get_action_meanings(), env.action_space.n)
 
 max_num_memories = 1000000
 memory_storage = RingBuf(max_num_memories)
 
 warmup_size = 50000
-model.warmup(env, warmup_size, memory_storage)
+#model.warmup(env, warmup_size, memory_storage)
 
 is_done = False
 for i in range(1000000):
@@ -22,18 +22,20 @@ for i in range(1000000):
   model.state_list = [frame]
   is_done = False
   while not is_done:
-    for j in range(4):
-      # FIXME choose best action
-      #action = env.action_space.sample()
-      if (len(model.state_list)<4):
-        action = env.action_space.sample()
-      #else:
-        #action = model.choose_best_action([model.state_list[len(model.state_list)-4:len(model.state_list)], np.ones(env.action_space.shape)])
+    for j in range(3):
+      action = env.action_space.sample()
       new_frame, reward, is_done, _ = env.step(action)
-      if is_done:
-        break
       model.state_list.append(preprocess(new_frame))
+      env.render()
 
-    is_done = model.q_iteration(env, model.state_list[len(model.state_list)-4:len(model.state_list)], i, memory_storage)
+    action = model.choose_best_action([model.previous_state()])
+    new_frame, reward, is_done, _ = env.step(action)
+    env.render()
+
+    if is_done:
+      break
+    
+    model.state_list.append(preprocess(new_frame))
+    is_done = model.q_iteration(env, model.previous_state(), i, memory_storage)
 
 env.close()
